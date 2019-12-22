2187af4ec9a4404e14d0ac34500446eeae41ffca
un950 <un950@jiwoonjeong>
2019-12-22 19:33:43 +0900

    공급자목록보기

--- YORIJORI/src/main/java/Controller/Manager/ProviderController.java
+++ YORIJORI/src/main/java/Controller/Manager/ProviderController.java
@@ -2,10 +2,12 @@ package Controller.Manager;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import Command.manager.ProRegistCommand;
+import Service.Manager.ProListService;
 import Service.Manager.ProRegistService;
 
 @Controller
@@ -14,6 +16,8 @@ public class ProviderController {
 	@Autowired
 	ProRegistService proRegistService;
 	
+	@Autowired
+	ProListService proListService;
 	//공급자 등록 페이지 이동
 	@RequestMapping("/providerRegistration")
 	public String providerRegistration(ProRegistCommand proRegistCommand) {
@@ -26,5 +30,13 @@ public class ProviderController {
 		Integer result = proRegistService.regist(proRegistCommand);
 		return "manager/providerList";
 	}
+	//공급자 목록 페이지 이동
+	@RequestMapping("/providerList")
+	public String providerList(Model model) {
+		//Service 실행.
+		proListService.list(model);
+		return "manager/providerList";
+	}
+		
 	
 }
